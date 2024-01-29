7984a075acfb0b0b0b4fc5c56cacf6a0bcf4efc4
Matthew <mrmanka0307@gmail.com>
2024-01-29 05:12:21 -0600

    Added graceful retry

--- src/CrashEventFilter.ts
+++ src/CrashEventFilter.ts
@@ -4,8 +4,21 @@ import * as os from "os";
 
 export async function FilterEvents() {
   let path: string = "C:\\temp";
-  let logs: any = await getData(`${path}\\PalWorldServerCrashLog.txt`);
-  let logJSON: any = await parseLogToJSON(logs);
+  let logs: any = void 0;
+  let logJSON: any = void 0;
+  let attempt: number = 0;
+
+  while (true) {
+    try {
+      logs = await getData(`${path}\\PalWorldServerCrashLog.txt`);
+      logJSON = await parseLogToJSON(logs);
+      break;
+    } catch (err) {
+      if (attempt > 3) throw err;
+      else attempt++;
+    }
+  }
+  if(logs && logJSON.length === 0) throw `Failed to parse logs into JSON`;
   const PalServerErrorLogs: any = logJSON.filter((log: any) => {
     return /PalSer\w{0,3}.{0,19}/gi.test(log.P1);
   });
