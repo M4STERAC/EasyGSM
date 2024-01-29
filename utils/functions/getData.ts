fe81915da5ccce655943c872108ebfdec5456c64
Matthew <mrmanka0307@gmail.com>
2024-01-29 05:12:41 -0600

    ooooo pretty

--- utils/functions/getData.ts
+++ utils/functions/getData.ts
@@ -1,21 +1,20 @@
-import * as fs from 'fs';
+import * as fs from "fs";
 
 export async function getData(path: string) {
-    let attempt: number = 0;
-    let logs: any;
+  let attempt: number = 0;
+  let logs: any;
 
-    while (true) {
-        try { 
-            let logsBuffer: Buffer = fs.readFileSync(path);
-            logs = logsBuffer.toString('utf-8');
-            break;
-        }
-        catch (err) { 
-            attempt++;
-            if (attempt > 3) throw `Failed to get logs from ${path}`;
-            console.error('Failed to get logs. Retrying...');
-        }
+  while (true) {
+    try {
+      let logsBuffer: Buffer = fs.readFileSync(path);
+      logs = logsBuffer.toString("utf-8");
+      break;
+    } catch (err) {
+      attempt++;
+      if (attempt > 3) throw `Failed to get logs from ${path}`;
+      console.error("Failed to get logs. Retrying...");
     }
-    if(!logs) throw 'Log file is empty';
-    return logs;
+  }
+  if(!logs) throw 'Log file is empty';
+  return logs;
 }
