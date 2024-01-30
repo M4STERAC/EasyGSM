bac0a0087360ac0407fd132265d7a237959ed331
Matthew <mrmanka0307@gmail.com>
2024-01-29 22:44:03 -0600

    fixed bug on backup manager second target path

--- src/BackupManager.bat
+++ src/BackupManager.bat
@@ -3,10 +3,19 @@ for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /format:list') do 
 set currentDate=%datetime:~0,4%\\%datetime:~4,2%\\%datetime:~6,2%
 
 set targetPath=%USERPROFILE%\AppData\Local\Pal\Saved\SaveGames\
+set secondTargetPath=C:\Program Files (x86)\Steam\steamapps\common\PalServer\Pal\Saved\SaveGames\
 set destinationPath=%USERPROFILE%\Documents\PalServerBackups
 
 if not exist "%destinationPath%\%currentDate%" mkdir "%destinationPath%\%currentDate%"
 if not exist "%destinationPath%\" mkdir "%destinationPath%"
+if not exist "%targetPath%" (
+    if not exist "%secondTargetPath%" (
+        echo "PalWorld saves not found at native path. Please move saves to their native directory at %targetPath%"
+        pause
+        exit
+    )
+    powershell -command "Compress-Archive -Path '%secondTargetPath%*' -DestinationPath '%destinationPath%\%currentDate%\WorldSaveBackups.zip'"
+)
 if not exist "%targetPath%" echo "PalWorld saves not found at native path. Please move saves to their native directory at %targetPath%"
 
 powershell -command "Compress-Archive -Path '%targetPath%*' -DestinationPath '%destinationPath%\%currentDate%\WorldSaveBackups.zip'"
