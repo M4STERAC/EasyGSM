9b641e6e4473864f4cf6dcc4d8d2815a2c418feb
Matthew <mrmanka0307@gmail.com>
2024-01-29 21:51:32 -0600

    BackupManager now works

--- src/BackupManager.bat
+++ src/BackupManager.bat
@@ -1,6 +1,6 @@
 @echo off
 for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /format:list') do set datetime=%%I
-set currentDate=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2%
+set currentDate=%datetime:~0,4%\\%datetime:~4,2%\\%datetime:~6,2%
 
 set targetPath=%USERPROFILE%\AppData\Local\Pal\Saved\SaveGames\
 set destinationPath=%USERPROFILE%\Documents\PalServerBackups
@@ -10,5 +10,4 @@ if not exist "%destinationPath%\" mkdir "%destinationPath%"
 if not exist "%targetPath%" echo "PalWorld saves not found at native path. Please move saves to their native directory at %targetPath%"
 
 powershell -command "Compress-Archive -Path '%targetPath%*' -DestinationPath '%destinationPath%\%currentDate%\WorldSaveBackups.zip'"
-pause
 exit
