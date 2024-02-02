3a2a49ed69433d11f0634400bc47ae35f634ef0f
Matthew <mrmanka0307@gmail.com>
2024-02-02 00:44:53 -0600

    edited server start unit test

--- .test/src/ui/index.test.js
+++ .test/src/ui/index.test.js
@@ -5,7 +5,7 @@ import App from '../src/ui/App';
 describe("React - Happy Path", () => {
     test('renders the React app without crashing', () => {
         const { getByText } = render(<App />);
-        const linkElement = getByText(/Welcome to EasyGSM/i);
+        const linkElement = getByText(/SERVER LIST/i);
         expect(linkElement).toBeInTheDocument();
     }, 500);
 })
