c7481bc46dcc6ddecebc86c4d62a92e48a32bed7
Matthew <mrmanka0307@gmail.com>
2024-02-02 15:37:53 -0600

    Renamed files

--- src/ui/utils/generalFunctions.js
+++ src/ui/utils/generalFunctions.js
@@ -1,4 +1,4 @@
-function createUTCDate() {
+export async function createUTCDate() {
     const date = new Date();
     const year = date.getUTCFullYear();
     const month = String(date.getUTCMonth() + 1).padStart(2, '0');
