1d877755fd7f188fbf83da31186f1aaae6592d2b
Matthew <mrmanka0307@gmail.com>
2024-02-01 23:07:23 -0600

    importing react

---
+++ src/ui/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div>
+    <p>404. The route you entered could not be found. Sorry !</p>
+    <p>EasyGSM Team</p>
+  </div>
+);
+
+export default NotFound;
