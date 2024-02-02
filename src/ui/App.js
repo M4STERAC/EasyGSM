69fcd3bc2d29b4c48752805faf6dceff78cb3f7a
Matthew <mrmanka0307@gmail.com>
2024-02-02 16:12:07 -0600

    Added sentry unit

--- src/ui/App.js
+++ src/ui/App.js
@@ -1,15 +1,26 @@
 import React from "react";
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 // import NotFound from "./pages/NotFound";
 import "./css/General.css";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/browser";
 
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0, // Adjust this value to control the percentage of transactions sent
+});
+
+console.log("loaded app.js");
 const App = () => (
-  <HashRouter>
-    <Route exact path="/" component={MainPage} />
-    {/* <Route component={NotFound} /> */}
-  </HashRouter>
+  <Sentry.ErrorBoundary fallback={"An error has occurred"}>
+    <HashRouter>
+      <Route exact path="/" component={MainPage} />
+      {/* <Route component={NotFound} /> */}
+    </HashRouter>
+  </Sentry.ErrorBoundary>
 );
 
 export default App;
