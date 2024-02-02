d9572062b345d48c99036fc080032a952aaf843c
Matthew <mrmanka0307@gmail.com>
2024-02-02 15:38:56 -0600

    removed styling

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './css/general.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App className="general-style"/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
