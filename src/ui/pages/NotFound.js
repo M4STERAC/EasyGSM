a31448b249a183b9436cec27cc615e4187407044
Matthew <mrmanka0307@gmail.com>
2024-02-02 15:38:51 -0600

    Added link back to main page

--- src/ui/pages/NotFound.js
+++ src/ui/pages/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => (
   <div>
     <p>404. The route you entered could not be found. Sorry !</p>
     <p>EasyGSM Team</p>
+    <Link to="/">Back to Main</Link>
   </div>
 );
 
