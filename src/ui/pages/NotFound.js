6ac1e36a015fcb79ed49c226109dc7ddffe0e2d6
Matthew <mrmanka0307@gmail.com>
2024-02-02 16:12:37 -0600

    added console log on page load

--- src/ui/pages/NotFound.js
+++ src/ui/pages/NotFound.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+console.log('loaded NotFound.js')
 const NotFound = () => (
   <div>
     <p>404. The route you entered could not be found. Sorry !</p>
