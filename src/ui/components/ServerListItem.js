edbaa6ea47c83f8d421a8ca2e90c96e0c6f98f80
Matthew <mrmanka0307@gmail.com>
2024-02-03 00:04:34 -0600

    Created component to render server info

---
+++ src/ui/components/ServerListItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../css/ServerListItem.css";
+
+const ServerListItem = (server) => {
+  return (
+    <li>
+      <a href="javascript:void(0)">
+        {server.game} - {server.name}{" "}
+        <span
+          className={
+            server.status === "Running" ? "running" : server.status === "Ready" ? "ready" : "down"
+          }>
+          {server.status}
+        </span>
+      </a>
+    </li>
+  );
+};
+
+export default ServerListItem;
