7068382b9e9e8b130af92eabc8ee148700f1d342
Matthew <mrmanka0307@gmail.com>
2024-02-03 01:05:56 -0600

    converted to js

--- src/data/CrashEventFilter.js
+++ src/data/CrashEventFilter.js
@@ -3,14 +3,14 @@ import { parseLogToJSON } from "./utils/functions";
 import * as os from "os";
 
 export async function FilterEvents() {
-  let path: string = "C:\\temp\\EasyGSM\\PalWorldServerCrashLog.txt";
-  let logs: any = void 0;
-  let logJSON: any = void 0;
-  let attempt: number = 0;
+  let path = "C:\\temp\\EasyGSM\\PalWorldServerCrashLog.txt";
+  let logs = void 0;
+  let logJSON = void 0;
+  let attempt = 0;
 
   while (true) {
     try {
-      let logsBuffer: Buffer = fs.readFileSync(path);
+      let logsBuffer = fs.readFileSync(path);
       logs = logsBuffer.toString("utf-8");
       logJSON = await parseLogToJSON(logs);
       break;
@@ -20,7 +20,7 @@ export async function FilterEvents() {
     }
   }
   if (logs && logJSON.length === 0) throw `Failed to parse logs into JSON`;
-  const PalServerErrorLogs: any = logJSON.filter((log: any) => { return /PalSer\w{0,3}.{0,19}/gi.test(log.P1) });
+  const PalServerErrorLogs = logJSON.filter((log) => { return /PalSer\w{0,3}.{0,19}/gi.test(log.P1) });
   fs.writeFileSync(
     `${os.homedir()}\\Documents\\EasyGSM\\PalServer\\logs\\error\\PalServerErrorLogs.json`,
     JSON.stringify(PalServerErrorLogs)
