4febc1fa709bf0f629d0eee4f7f56e7c466165ca
Matthew <mrmanka0307@gmail.com>
2024-02-03 00:31:06 -0600

    made main.ts into a js app

--- src/ui/main.js
+++ src/ui/main.js
@@ -1,5 +1,12 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const { ipcMain } = require('electron');
+const fs = require('fs');
+
+ipcMain.handle('read-json', async (event, jsonFilePath) => {
+  const data = fs.readFileSync(path.resolve(__dirname, jsonFilePath), 'utf-8');
+  return JSON.parse(data);
+});
 
 function createWindow () {
   const win = new BrowserWindow({
