9123809138f748b4a427ff013596912877263d82
Matthew <mrmanka0307@gmail.com>
2024-02-03 00:03:48 -0600

    Removed serve and json-server

--- src/ui/utils/api.js
+++ src/ui/utils/api.js
@@ -1,12 +1,28 @@
-import axios from "axios";
+import fs from "fs";
 
-export async function getAllServerData(address) {
+export async function getAllServers(jsonFile) {
   for (let i = 0; i < 3; i++) {
     try {
-      const response = await axios.get(address);
-      return response.data;
+      const servers = JSON.parse(fs.readFileSync(jsonFile, 'utf-8'));
+      console.log(servers);
+      return servers;
     } catch (error) {
       console.error("Error fetching server data:", error);
+      console.error("Retrying...");
+    }
+  }
+}
+
+export async function addServer(data) {
+  for (let i = 0; i < 3; i++) {
+    try {
+      const servers = fs.readFileSync(jsonFile).toString("utf-8");
+      console.log(servers);
+      servers.push(data);
+      const updatedJson = JSON.stringify(servers, null, 2);
+      return { statusCode: 200, data: updatedJson }
+    } catch (error) {
+      console.error("Error adding server:", error);
       console.error("Retrying...");
     }
   }
