f03ab43c535e18079f6b173a63a49eca850d64a8
Matthew <mrmanka0307@gmail.com>
2024-02-05 17:05:25 -0600

    added button animations

--- src/ui/components/ServerInfoItem.js
+++ src/ui/components/ServerInfoItem.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { CSSTransition } from "react-transition-group";
 import { Link } from "react-router-dom";
 import "../css/ServerInfoItem.css";
 
 const ServerInfoItem = ({ selectedServer }) => {
+  const [buttonState, setButtonState] = useState(selectedServer.status);
+  useEffect(() => {
+    setButtonState(selectedServer.status);
+  }, [selectedServer.status]);
+
   return (
     <div>
       <li>
@@ -14,10 +20,44 @@ const ServerInfoItem = ({ selectedServer }) => {
         <p>Uptime: {selectedServer.uptime}</p>
       </li>
       <li>
-        <Link to={{ pathname: "/update-server", state: { yourState: selectedServer }}} j>
+        <Link
+          to={{
+            pathname: "/update-server",
+            state: { yourState: selectedServer },
+          }}
+          j
+        >
           Edit Configuration
         </Link>
       </li>
+      <div className="button-container">
+        <CSSTransition
+          in={buttonState === "Down"}
+          timeout={300}
+          classNames="start-button"
+          unmountOnExit
+        >
+          <button
+            className="start-button"
+            onClick={() => setButtonState("Running")}
+          >
+            Start
+          </button>
+        </CSSTransition>
+        <CSSTransition
+          in={buttonState !== "Down"}
+          timeout={300}
+          classNames="stop-button"
+          unmountOnExit
+        >
+          <button
+            className="stop-button"
+            onClick={() => setButtonState("Down")}
+          >
+            Stop
+          </button>
+        </CSSTransition>
+      </div>
     </div>
   );
 };
