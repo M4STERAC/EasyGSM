98e3766f31a48ab95fcf150389332ff5da72552a
Matthew <mrmanka0307@gmail.com>
2024-02-05 15:15:36 -0600

    added file loader for images ang copy webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,32 @@
-const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { sentryWebpackPlugin } = require("@sentry/webpack-plugin");
-const webpack = require('webpack');
-require('dotenv').config();
+const webpack = require("webpack");
+const path = require("path");
+require("dotenv").config();
 
 module.exports = {
-  devtool: "source-map", // Sentry SourceMap upload
-  target: 'electron-renderer',
+  devtool: "source-map",
+  target: "electron-renderer",
   entry: "./src/ui/index.js",
   module: {
     rules: [
       {
-        test: /\.(ts|tsx|js|jsx)$/,
+        test: /\.(ts|tsx|js|jsx|json)$/,
         exclude: /node_modules/,
         use: ["babel-loader"],
       },
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
       },
     ],
   },
@@ -35,12 +44,18 @@ module.exports = {
     sentryWebpackPlugin({
       org: "easygsm",
       project: "easygsm",
-      // authToken: process.env.SENTRY_AUTH_TOKEN,
       authToken: process.env.SENTRY_AUTH_TOKEN,
     }),
     new webpack.DefinePlugin({
-      'process.env.REACT_APP_SENTRY_DSN': JSON.stringify(process.env.REACT_APP_SENTRY_DSN),
-      'process.env.SENTRY_AUTH_TOKEN': JSON.stringify(process.env.SENTRY_AUTH_TOKEN),
+      "process.env.REACT_APP_SENTRY_DSN": JSON.stringify(
+        process.env.REACT_APP_SENTRY_DSN
+      ),
+      "process.env.SENTRY_AUTH_TOKEN": JSON.stringify(
+        process.env.SENTRY_AUTH_TOKEN
+      ),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "src/data", to: "../data" }],
     }),
   ],
 };
