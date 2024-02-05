5a6332032eebf6d641545b05d7f52072b92a3cc7
Matthew <mrmanka0307@gmail.com>
2024-02-05 01:14:05 -0600

    Got api call to work for json-server

--- src/ui/pages/MainPage.js
+++ src/ui/pages/MainPage.js
@@ -1,82 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "../components/Card";
 import Footer from "../components/Footer";
+import ServerListItem from "../components/ServerListItem";
+import ServerInfoItem from "../components/ServerInfoItem";
 import "../css/General.css";
 import "../css/MainPage.css";
 
-console.log("loaded MainPage.js");
-const MainPage = () => (
-  <div className="general-style">
-    <div className="content">
-      <Card>
-        <h2 className="card-title">SERVER LIST</h2>
-        <ul className="clickable-list">
-          <li>
-            <a href="#">PalServer - Public World </a>{" "}
-            <span className="running">Running </span>
-          </li>
-          <li>
-            <a href="#">PalServer - YouTube World </a>{" "}
-            <span className="running">Running </span>
-          </li>
-          <li>
-            <a href="#">PalServer - Friend Group </a>{" "}
-            <span className="down"> Down </span>
-          </li>
-          <li>
-            <a href="#">PalServer - Solo </a> <span className="running">Running </span>
-          </li>
-          <li>
-            <a href="#">Gary's Mod - Prop Hunt </a>{" "}
-            <span className="down"> Down </span>
-          </li>
-          <li>
-            <a href="#">Gary's Mod - Open Public </a>{" "}
-            <span className="running">Running </span>
-          </li>
-          <li>
-            <a href="#">Gary's Mod - Private </a>{" "}
-            <span className="running">Running </span>
-          </li>
-          <li>
-            <a href="#">Minecraft - Underground City</a>{" "}
-            <span className="running">Running </span>
-          </li>
-          <li>
-            <a href="#">Minecraft - Sky Blocks </a>{" "}
-            <span className="ready"> Ready </span>
-          </li>
-        </ul>
-        <button className="add-server">+</button>
-      </Card>
-      <Card>
-        <h2>SERVER INFO</h2>
-        <ul className="clickable-list">
-          <li>
-            <a href="#">Uptime: 15 Days</a>
-          </li>
-          <li>
-            <a href="#">Edit Ban List</a>
-          </li>
-          <li>
-            <a href="#">Edit Configuration</a>
-          </li>
-        </ul>
-      </Card>
-    </div>
-    <Footer>
-      <div>
-        <div className="footer-logo">
-          <img src="../images/EasyGSM Logo.png" alt="EasyGSM Logo" />
-        </div>
-        <div className="footer-text">
-          EasyGSM is an open source software. Please credit
-          https://github.com/M4STERAC/EasyGSM as the first author of the program
-          in all areas where credit is due.
-        </div>
+console.debug("loaded MainPage.js");
+const MainPage = () => {
+  const [serverList, setServerList] = useState([]);
+  const [selectedServer, setSelectedServer] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/Server")
+      .then((response) => response.json())
+      .then((data) => {
+        console.debug(data);
+        setServerList(data);
+      })
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
+  useEffect(() => {
+    console.debug("selectedServer changed");
+    console.debug(selectedServer);
+  }, [selectedServer]);
+
+  return (
+    <div className="general-style">
+      <div className="content">
+        <Card>
+          <h2 className="card-title">SERVER LIST</h2>
+          <ul className="clickable-list">
+            {serverList.map((server, index) => (
+              <ServerListItem onClick={() => {
+                console.log('CLICKED')
+                setSelectedServer(server);
+                console.log(selectedServer);
+              }} key={index} server={server} />
+            ))}
+          </ul>
+          <button className="add-server">+</button>
+        </Card>
+        <Card>
+          <h2 className="card-title">SERVER INFO</h2>
+          <ul className="clickable-list">
+            {selectedServer ? <ServerInfoItem selectedServer={selectedServer} /> : <li>Select a server</li>}
+          </ul>
+        </Card>
       </div>
-    </Footer>
-  </div>
-);
+      <Footer>
+        <div>
+          <div className="footer-logo">
+            <img src="./images/EasyGSMLogo.png" alt="EasyGSM Logo" />
+          </div>
+          <div className="footer-text">
+            EasyGSM is an open source software. Please credit
+            https://github.com/M4STERAC/EasyGSM as the first author of the
+            program in all areas where credit is due.
+          </div>
+        </div>
+      </Footer>
+    </div>
+  );
+};
 
 export default MainPage;
