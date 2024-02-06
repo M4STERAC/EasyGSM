73b761123409223bc841058d61e57bdc623d3225
Matthew <mrmanka0307@gmail.com>
2024-02-05 22:57:48 -0600

    commented out useeffect

--- src/ui/Store.js
+++ src/ui/Store.js
@@ -1,14 +1,21 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, /* useEffect */ } from "react";
 
 export const StoreContext = createContext();
 
 export const StoreProvider = ({ children }) => {
   const [state, setState] = useState({
     // Your initial state here
-    serverList: [],
   });
   console.log("Store Loaded");
-  
+
+  // useEffect(() => {
+  //   fetch("http://localhost:3001/Server")
+  //     .then((response) => response.json())
+  //     .then((data) =>
+  //       setState((prevState) => ({ ...prevState, serverList: data }))
+  //     )
+  //     .catch((error) => console.error("Error:", error));
+  // }, []);
 
   return (
     <StoreContext.Provider value={[state, setState]}>
