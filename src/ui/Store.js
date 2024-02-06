f1b2ba0a4160a96195fc7559e3959e1fc266ef5a
Matthew <mrmanka0307@gmail.com>
2024-02-05 18:23:31 -0600

    added default state

--- src/ui/Store.js
+++ src/ui/Store.js
@@ -1,11 +1,14 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState } from "react";
 
 export const StoreContext = createContext();
 
 export const StoreProvider = ({ children }) => {
   const [state, setState] = useState({
     // Your initial state here
+    serverList: [],
   });
+  console.log("Store Loaded");
+  
 
   return (
     <StoreContext.Provider value={[state, setState]}>
