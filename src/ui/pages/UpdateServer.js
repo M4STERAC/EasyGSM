9bdce0702196e450fd1f5151dedda094ca06627d
Matthew <mrmanka0307@gmail.com>
2024-02-05 18:22:49 -0600

    Delete is now working

--- src/ui/pages/UpdateServer.js
+++ src/ui/pages/UpdateServer.js
@@ -1,21 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { createUTCDate } from "../utils/generalFunctions";
+import { StoreContext } from "../Store";
 import { useNavigate } from "react-router-dom";
 import Card from "../components/Card";
 import "../css/CreateServer.css";
 
 const UpdateServer = () => {
-  // const location = useLocation();
-  // const selectedServer = location.state.selectedServer;
-  //temp until state starts working
-  const selectedServer = {
-    game: "",
-    name: "",
-    executable: "",
-    saveDirectory: "",
-    banlist: [],
-  };
   const navigate = useNavigate();
+  const [state, setState] = useContext(StoreContext);
   const [game, setGame] = useState("");
   const [name, setName] = useState("");
   const [executable, setExecutable] = useState("");
@@ -32,7 +24,7 @@ const UpdateServer = () => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        // selectedServer.id,
+        id: state.selectedServer.id,
         game,
         name,
         executable,
@@ -54,91 +46,112 @@ const UpdateServer = () => {
       });
   };
 
+  const deleteServer = () => {
+    console.log(state.selectedServer.id);
+    fetch(`http://localhost:3001/Server/${state.selectedServer.id}`, {
+      method: "DELETE"
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .then(() => navigate("/"))
+      .catch((error) => {
+        console.error("Error:", error);
+        setUpdateFail(true);
+      });
+  };
+
   return (
     <Card>
-      {/* { !selectedServer ? <p className="error">Failed to get Server Configuration. Please try again later.</p> : */}
-      <div>
-      <h2 className="card-title">Update Server</h2>
-        <form onSubmit={handleSubmit} className="server-form">
-          <label>Game:</label>
-          <input
-            type="text"
-            value={game}
-            onChange={(e) => setGame(e.target.value)}
-            placeholder={selectedServer.game}
-          />
-          <br />
-          <label>Name:</label>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            placeholder={selectedServer.name}
-          />
-          <br />
-          <label>Path to Game Executable:</label>
-          <input
-            type="file"
-            webkitdirectory=""
-            directory=""
-            multiple
-            onChange={(e) => {
-              if (e.target.files.length > 0) {
-                const path = e.target.files[0].webkitRelativePath
-                  .split("/")
-                  .slice(0, -1)
-                  .join("/");
-                setExecutable(path);
-              }
-            }}
-            placeholder={selectedServer.executable}
-          />
-          <br />
-          <label>Save Directory:</label>
-          <input
-            type="file"
-            webkitdirectory=""
-            directory=""
-            multiple
-            onChange={(e) => {
-              if (e.target.files.length > 0) {
-                const path = e.target.files[0].webkitRelativePath
-                  .split("/")
-                  .slice(0, -1)
-                  .join("/");
-                setSaveDirectory(path);
-              }
-            }}
-            placeholder={selectedServer.saveDirectory}
-          />
-          <br />
-          <label>Banlist:</label>
-          <input
-            type="text"
-            value={banlist}
-            onChange={(e) => setBanlist(e.target.value)}
-            placeholder={selectedServer.banlist.toString()}
-          />
-          <br />
-          {updateFail ? (
-            <p className="error">
-              Failed to update server. Please validate input data.
-            </p>
-          ) : null}
-          <div className="button-container">
-            <button type="submit" className="submit-button">
-              Create
-            </button>
-            <button className="cancel-button" onClick={() => navigate("/")}>
-              Cancel
-            </button>
-            <button className="delete-button" onClick={() => navigate("/")}>
-              Delete
-            </button>
-          </div>
-        </form>
-      </div>
-      {/* } */}
+      {!state.selectedServer ? (
+        <p className="error">
+          Failed to get Server Configuration. Please try again later.
+        </p>
+      ) : (
+        <div>
+          <h2 className="card-title">Update Server</h2>
+          <form onSubmit={handleSubmit} className="server-form">
+            <label>Game:</label>
+            <input
+              type="text"
+              value={game}
+              onChange={(e) => setGame(e.target.value)}
+              placeholder={state.selectedServer.game}
+            />
+            <br />
+            <label>Name:</label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder={state.selectedServer.name}
+            />
+            <br />
+            <label>Path to Game Executable:</label>
+            <input
+              type="file"
+              webkitdirectory=""
+              directory=""
+              multiple
+              onChange={(e) => {
+                if (e.target.files.length > 0) {
+                  const path = e.target.files[0].webkitRelativePath
+                    .split("/")
+                    .slice(0, -1)
+                    .join("/");
+                  setExecutable(path);
+                }
+              }}
+              placeholder={state.selectedServer.executable}
+            />
+            <br />
+            <label>Save Directory:</label>
+            <input
+              type="file"
+              webkitdirectory=""
+              directory=""
+              multiple
+              onChange={(e) => {
+                if (e.target.files.length > 0) {
+                  const path = e.target.files[0].webkitRelativePath
+                    .split("/")
+                    .slice(0, -1)
+                    .join("/");
+                  setSaveDirectory(path);
+                }
+              }}
+              placeholder={state.selectedServer.saveDirectory}
+            />
+            <br />
+            <label>Banlist:</label>
+            <input
+              type="text"
+              value={banlist}
+              onChange={(e) => setBanlist(e.target.value)}
+              placeholder={state.selectedServer.banlist.toString()}
+            />
+            <br />
+            {updateFail ? (
+              <p className="error">
+                Failed to update/delete server. Please validate input data.
+              </p>
+            ) : null}
+            <div className="button-container">
+              <button type="submit" className="submit-button">
+                Create
+              </button>
+              <button className="cancel-button" onClick={() => navigate("/")}>
+                Cancel
+              </button>
+              <button
+                className="delete-button"
+                onClick={() => deleteServer()}
+              >
+                Delete
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
     </Card>
   );
 };
