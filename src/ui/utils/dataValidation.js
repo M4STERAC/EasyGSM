c32e436b9e06ea547a32c25b1ba933cd552f333a
Matthew <mrmanka0307@gmail.com>
2024-02-11 13:47:59 -0600

    validates empty strings for IP

--- src/ui/utils/dataValidation.js
+++ src/ui/utils/dataValidation.js
@@ -1,5 +1,6 @@
 export const validateIpAddress = (ip) => {
   console.log(`IP Address ${ip} is being validated`);
+  if (ip === "") return true;
   const regex = new RegExp(
     /^(\d{1,2}|0\d{0,2}|1\d{0,2}|2[0-4]\d|2[0-5][0-5])\.(\d{1,2}|0\d{0,2}|1\d{0,2}|2[0-4]\d|2[0-5][0-5])\.(\d{1,2}|0\d{0,2}|1\d{0,2}|2[0-4]\d|2[0-5][0-5])\.(\d{1,2}|0\d{0,2}|1\d{0,2}|2[0-4]\d|2[0-5][0-5])$/gm
   );
