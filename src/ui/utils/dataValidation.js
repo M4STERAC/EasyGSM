c9b1e8cbd0325cd455d1042be9a0f62b6a570859
Matthew <mrmanka0307@gmail.com>
2024-02-11 13:45:44 -0600

    Correctly validates input for IP

--- src/ui/utils/dataValidation.js
+++ src/ui/utils/dataValidation.js
@@ -1,7 +1,8 @@
 export const validateIpAddress = (ip) => {
-  console.log(ip);
+  console.log(`IP Address ${ip} is being validated`);
   const regex = new RegExp(
-    "^(d{0,2}|0d{0,2}|1d{0,2}|2[0-4]d|2[0-5][0-5]).(d{0,2}|0d{0,2}|1d{0,2}|2[0-4]d|2[0-5][0-5]).(d{0,2}|0d{0,2}|1d{0,2}|2[0-4]d|2[0-5][0-5]).(d{0,2}|0d{0,2}|1d{0,2}|2[0-4]d|2[0-5][0-5])$"
+    /^(\d{1,2}|0\d{0,2}|1\d{0,2}|2[0-4]\d|2[0-5][0-5])\.(\d{1,2}|0\d{0,2}|1\d{0,2}|2[0-4]\d|2[0-5][0-5])\.(\d{1,2}|0\d{0,2}|1\d{0,2}|2[0-4]\d|2[0-5][0-5])\.(\d{1,2}|0\d{0,2}|1\d{0,2}|2[0-4]\d|2[0-5][0-5])$/gm
   );
-  return regex.test(ip);
+  const result = regex.test(ip);
+  return result;
 };
