a2297d01c49202ed3217b3c40c79587176730d8b
Matthew <mrmanka0307@gmail.com>
2024-02-12 17:07:23 -0600

    created task will now call with arguments

--- src/data/CreateBackupSchedule.bat
+++ src/data/CreateBackupSchedule.bat
@@ -1,16 +1,16 @@
 @echo off
 
 set game=%~1
-@REM xx:xx format. Example 06:00
 set time=%~2
+set saveDirectory=%~3
 
 schtasks /query /tn "%game%BackupSchedule" >nul 2>&1
 if errorlevel 1 (
     echo The task does not exist.
-    schtasks /create /sc daily /tn "%game%PalServerBackupSchedule" /tr "%~dp0BackupManager.bat" /st %time%
+    schtasks /create /sc daily /tn "%game%BackupSchedule" /tr "%~dp0BackupManager.bat" /st %time%
 ) else (
     echo The task exists, deleting now...
-    schtasks /delete /tn "%game%PalServerBackupSchedule" /f
-    schtasks /create /sc daily /tn "%game%PalServerBackupSchedule" /tr "%~dp0BackupManager.bat" /st %time%
+    schtasks /delete /tn "%game%BackupSchedule" /f
+    schtasks /create /sc daily /tn "%game%BackupSchedule" /tr "%~dp0BackupManager.bat %game% %saveDirectory%" /st %time%
 )
 pause
