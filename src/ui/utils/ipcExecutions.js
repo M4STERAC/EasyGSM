ef33b2b5b45264051085cec8d6c8098aa74d4a77
Matthew <mrmanka0307@gmail.com>
2024-02-12 22:14:39 -0600

    deleted unneeded file

--- src/ui/utils/ipcExecutions.js
+++
@@ -1,5 +0,0 @@
-export const executeScript = (scripts) => {
-  window.electron.invoke("execute-script", scripts)
-  .then((result) => console.log(result))
-  .catch((error) => console.error(error));
-}
