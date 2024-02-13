38016b8c776bbc8091eea89f4773244211fce716
Matthew <mrmanka0307@gmail.com>
2024-02-13 16:49:44 -0600

    now accepts arguments

--- src/data/CreateBackupSchedule.bat
+++ src/data/CreateBackupSchedule.bat
@@ -7,7 +7,7 @@ set saveDirectory=%~3
 schtasks /query /tn "%game%BackupSchedule" >nul 2>&1
 if errorlevel 1 (
     echo The task does not exist.
-    schtasks /create /sc daily /tn "%game%BackupSchedule" /tr "%~dp0BackupManager.bat" /st %time%
+    schtasks /create /sc daily /tn "%game%BackupSchedule" /tr "%~dp0BackupManager.bat %game% %saveDirectory%" /st %time%
 ) else (
     echo The task exists, deleting now...
     schtasks /delete /tn "%game%BackupSchedule" /f
