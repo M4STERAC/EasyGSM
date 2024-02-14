bf69950844efa2653c5fd40b8b7ec8852e713a05
Matthew <mrmanka0307@gmail.com>
2024-02-13 19:50:26 -0600

    Now creates backup schedules based on saved servers

--- src/ui/Store.js
+++ src/ui/Store.js
@@ -6,15 +6,40 @@ export const StoreProvider = ({ children }) => {
   const [state, setState] = useState({
     serverList: [],
     selectedServer: null,
+    runningJobs: [],
   });
-  console.log("Store Loaded");
 
   useEffect(() => {
     window.electron
-      .invoke('get-data')
-      .then((data) => setState((prevState) => ({ ...prevState, serverList: data })))
-      .then(() => console.log('Database: ', state.serverList))
-      .catch((error) => console.error(error));
+      .invoke("get-data")
+      .then((servers) => {
+        setState((prevState) => ({ ...prevState, serverList: servers }));
+        console.log("Database: ", servers);
+        const schedulesToCreate = [];
+        servers.forEach((server) => {
+          if (
+            schedulesToCreate.includes({
+              source: server.saveDirectory,
+              game: server.game,
+              time: server.backuptime,
+            })
+          )
+            return;
+          schedulesToCreate.push({
+            source: server.saveDirectory,
+            game: server.game,
+            time: server.backuptime,
+          });
+        });
+        console.log("Schedules to create: ", schedulesToCreate);
+        schedulesToCreate.forEach((schedule) => {
+          window.electron
+            .invoke("create-schedule", schedule)
+            .then((data) => console.log(data))
+            .catch((error) => console.error("Create Schedule Error: ", error));
+        });
+      })
+      .catch((error) => console.error("Get Database Error: ", error));
   }, []);
 
   return (
