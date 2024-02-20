a6f80e9415e0e3c187ff5521dbf495ad0b2f270b
Matthew Manka <mrmanka0307@gmail.com>
2024-02-20 17:05:33 -0600

    added comments, jsdoc for functions, and cleaned up code

--- src/ui/utils/dataValidation.js
+++ src/ui/utils/dataValidation.js
@@ -1,90 +1,110 @@
-export const validateIpAddress = (banlist) => {
-  if (banlist === "") return true;
-  let error = "";
-  const ips = banlist.split(",");
-  const regex = new RegExp(
-    /^(\d{1,2}|0\d{0,2}|1\d{0,2}|2[0-4]\d|2[0-5][0-5])\.(\d{1,2}|0\d{0,2}|1\d{0,2}|2[0-4]\d|2[0-5][0-5])\.(\d{1,2}|0\d{0,2}|1\d{0,2}|2[0-4]\d|2[0-5][0-5])\.(\d{1,2}|0\d{0,2}|1\d{0,2}|2[0-4]\d|2[0-5][0-5])$/gm
-  );
+/**
+ * Validates a list of IP Addresses to ensure they are in the correct format and within the valid range
+ * @param {string} sanitizedBanlist List of IP addresses to validate
+ * @returns {true | string} - Returns true if the IP addresses in the string list is valid, returns an error message if it is not
+ */
+export const validateIpAddress = (sanitizedBanlist) => {
+  if (sanitizedBanlist === "") return true;
+  const ips = sanitizedBanlist.split(",");
+  const regex = new RegExp(/^(\d{1,2}|0\d{0,2}|1\d{0,2}|2[0-4]\d|2[0-5][0-5])\.(\d{1,2}|0\d{0,2}|1\d{0,2}|2[0-4]\d|2[0-5][0-5])\.(\d{1,2}|0\d{0,2}|1\d{0,2}|2[0-4]\d|2[0-5][0-5])\.(\d{1,2}|0\d{0,2}|1\d{0,2}|2[0-4]\d|2[0-5][0-5])$/gm);
   for (let ip of ips) {
     ip = ip.replaceAll(/[^\d\.]/gm, "").trim();
-    if (!regex.test(ip)) {
-      error = `IP Address: ${ip} is not valid. The max value for each octet is 255.`;
-      break;
-    } else {
-      console.log("IP Address: " + ip + " is valid");
-      error = "";
-    }
+    if (!regex.test(ip)) return `IP Address: ${ip} is not valid. The max value for each octet is 255.`;
   }
-  return error === "" ? true : error;
+  return true;
 };
 
-export const validatePort = (ports) => {
-  let error = "";
+
+/**
+ * Validates a list of ports to ensure they are within the valid range
+ * @param {string} sanitizedPorts List of ports to validate
+ * @returns {true | string} - Returns true if the ports in the string list is valid, returns an error message if it is not
+ */
+export const validatePort = (sanitizedPorts) => {
   if (
-    ports.tcpinbound === "" &&
-    ports.tcpoutbound === "" &&
-    ports.udpinbound === "" &&
-    ports.udpoutbound === ""
-  )
-    return true;
-  else {
-    const portsArray = Object.values(ports);
-    for (let portList of portsArray) {
-      const portSplit = portList.split(",");
-      for (let port of portSplit) {
-        port = port.replaceAll(/[^\d]/gm, "").trim();
-        console.log(`Port ${port} is being validated`);
-        if (+port < 0 || +port > 65535) {
-          error = `Invalid port ${port}. The max value for a port is 65535.`;
-          return error;
-        } else {
-          console.log(`Port: ${port} is valid`);
-          error = "";
-        }
-      }
+    sanitizedPorts.tcpinbound === "" &&
+    sanitizedPorts.tcpoutbound === "" &&
+    sanitizedPorts.udpinbound === "" &&
+    sanitizedPorts.udpoutbound === ""
+  ) return true;
+  const portsArray = Object.values(sanitizedPorts);
+  for (let portList of portsArray) {
+    const portSplit = portList.split(",");
+    for (let port of portSplit) {
+      port = port.replaceAll(/[^\d]/gm, "").trim();
+      if (+port < 0 || +port > 65535) return `Invalid port ${port}. The max value for a port is 65535.`;
     }
-    return error === "" ? true : error;
   }
+  return true;
 };
 
+
+/**
+ * Validates that the unique server id is not already in use
+ * @param {string[]} serverList Array of server objects pulled from electron store
+ * @param {string} id Generated ID to validate
+ * @returns {boolean} - Returns true if the ID is unique, false if it is not
+ */
 export const checkDuplicateIds = (serverList, id) => {
-  console.log(`Checking for duplicate id: ${id}`);
   const ids = serverList.map((server) => server.id);
-  const result = ids.includes(id);
-  console.log(
-    `Result: ${result ? "Found a" : "Did not find a"} duplicate id for ${id}`
-  );
   return ids.includes(id);
 };
 
+
+/**
+ * Validates that a file path is in the correct format
+ * @param {string} sanitizedPath File path to validate
+ * @returns {boolean} - Returns true if the path is valid, false if it is not
+ */
 export const validateFilePath = (sanitizedPath) => {
-  console.log(`Path ${sanitizedPath} is being validated`);
   if (sanitizedPath === "") return false;
   const unixRegex = new RegExp(/^\/(\/[a-zA-Z0-9-_\(\)\.]+)*$/gm);
   const windowsRegex = new RegExp(/^([A-Z]:)(\\[a-zA-Z0-9\s\(\)-_\.]+)*$/gm);
   const unixResult = unixRegex.test(sanitizedPath);
   const windowsResult = windowsRegex.test(sanitizedPath);
-  if (!windowsResult && !unixResult) {
-    console.log(
-      `Path ${sanitizedPath} is not a valid Unix or Windows file path`
-    );
-    return false;
-  }
+  if (!windowsResult && !unixResult) return false;
   return true;
 };
 
+
+/**
+ * Removes anything that isn't a letter, number or a space
+ * @param {string} input Input to sanitize
+ * @returns {string} Sanitized input
+ */
 export const sanitizeAlphanumeric = (input) => {
   return input.replaceAll(/[^a-z0-9 ]/gi, "");
 };
 
+
+/**
+ * Removes anything that doesn't resemble a filepath
+ * @param {string} input Input to sanitize
+ * @returns {string} Sanitized input
+ */
 export const sanitizeFilePath = (input) => {
-  return input.replaceAll(/[^a-z0-9\s\(\)\._\\:-]/gi, "");
+  //Windows
+  if (input.includes('\\')) return input.replaceAll(/[^a-z0-9\s\(\)\._\\:-]/gi, "");
+  //Unix
+  else return input.replaceAll(/[^a-z0-9\s\(\)\._/-]/gi, "");
 };
 
+
+/**
+ * Removes anything that isn't a number, comma or a space
+ * @param {string} input Input to sanitize
+ * @returns {string} Sanitized input
+ */
 export const sanitizePorts = (input) => {
   return input.replaceAll(/[^0-9,\s]/gi, "");
 };
 
+
+/**
+ * Removes anything that isn't a number, comma, period or a space
+ * @param {string} input Input to sanitize
+ * @returns {string} Sanitized input
+ */
 export const sanitizeIpAddress = (input) => {
   return input.replaceAll(/[^0-9.,\s]/gi, "");
 };
