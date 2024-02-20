ff98a011b0fb1c7f0d60894f78a42fd9bf24370b
Matthew Manka <mrmanka0307@gmail.com>
2024-02-20 17:05:17 -0600

    added comments

--- src/ui/pages/NotFound.js
+++ src/ui/pages/NotFound.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-console.log('loaded NotFound.js')
+//Default 404 page
 const NotFound = () => (
   <div>
-    <p>404. The route you entered could not be found. Sorry !</p>
-    <p>EasyGSM Team</p>
+    <p>404. The route you entered could not be found. Sorry!</p>
+    <p>-EasyGSM Team</p>
     <Link to="/">Back to Main</Link>
   </div>
 );
