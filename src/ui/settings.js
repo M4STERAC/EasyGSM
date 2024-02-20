bae01c0e40aa47f33b0bdfbd9b4ca54732a707ee
Matthew Manka <mrmanka0307@gmail.com>
2024-02-20 17:04:35 -0600

    added comments and cleaned up console logs

--- src/ui/settings.js
+++ src/ui/settings.js
@@ -1,7 +1,13 @@
 const store = require("electron-store");
 
+//Initialize electron store
 const storage = new store();
 
+/**
+ * Retrieves the window settings from storage.
+ * If no settings are found, default settings are set and returned.
+ * @returns {Object} The window settings object.
+ */
 function getWindowSettings() {
   const defaultBounds = { width: 800, height: 600 };
   const savedBounds = storage.get("windowBounds");
@@ -12,13 +18,19 @@ function getWindowSettings() {
   }
 }
 
+/**
+ * Saves the window bounds to storage.
+ * @param {Object} bounds - The window bounds object to save to electron store.
+ * @property {number} bounds.width - The window width.
+ * @property {number} bounds.height - The window height.
+ * @returns {undefined}
+ */
 function saveBounds(bounds) {
   try {
-    console.log("bounds: ", bounds);
     const newBounds = { width: bounds.width, height: bounds.height };
     storage.set("windowBounds", newBounds);
   } catch (error) {
-    console.log("error: ", error);
+    console.error("error: ", error);
   }
   return;
 }
