baf2caa8f026c7c35665bbc65d4eca5b85b03da4
Matthew Manka <mrmanka0307@gmail.com>
2024-02-20 17:05:45 -0600

    added comments, jsdoc, and cleaned up code

--- src/ui/utils/generalFunctions.js
+++ src/ui/utils/generalFunctions.js
@@ -1,3 +1,7 @@
+/**
+ * Returns a UTC date
+ * @returns {string} - Current date in the format of YYYY-MM-DD HH:MM:SS
+ */
 export async function createUTCDate() {
     const date = new Date();
     const year = date.getUTCFullYear();
@@ -10,15 +14,26 @@ export async function createUTCDate() {
     return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
 
-export function createDatePartition(os = 'windows') {
+
+/**
+ * Returns an OS-dependent-file-path partition of the current date in the format of YYYY-MM-DD
+ * @returns {string} - Date file path partition
+ */
+export function createDatePartition(os = "Windows") {
     const date = new Date();
     const year = date.getUTCFullYear();
     const month = String(date.getUTCMonth() + 1).padStart(2, '0');
     const day = String(date.getUTCDate()).padStart(2, '0');
 
-    return os === 'unix' ? `${year}/${month}/${day}` : `${year}\\${month}\\${day}`;
+    if (os === "Windows") return `${year}\\${month}\\${day}`;
+    else return `${year}/${month}/${day}`;
 }
 
+/**
+ * Generates a unique ID of the specified length
+ * @param {*} length Length of the ID to be created
+ * @returns {string} - Created Unique ID
+ */
 export function generateId(length) {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let result = '';
