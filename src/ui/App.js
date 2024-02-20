6253d47587a244f0143a163797dc16618c9cc488
Matthew Manka <mrmanka0307@gmail.com>
2024-02-20 17:02:47 -0600

    added a footer to the bottom of each loaded route

--- src/ui/App.js
+++ src/ui/App.js
@@ -3,19 +3,20 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import NotFound from "./pages/NotFound";
 import UpdateDatabase from "./pages/UpdateDatabase";
+import Footer from "./components/Footer";
 import "./css/General.css";
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/browser";
 import { StoreProvider } from "./Store";
 
+//Sentry setup for error tracking
 Sentry.init({
   dsn: process.env.REACT_APP_SENTRY_DSN,
   integrations: [new BrowserTracing()],
   tracesSampleRate: 1.0,
 });
 
-console.log("loaded app.js");
-
+//React Router setup
 const App = () => {
   return (
     <StoreProvider>
@@ -26,6 +27,7 @@ const App = () => {
           <Route path="/edit-server" element={<UpdateDatabase />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
+        <Footer />
       </Router>
     </StoreProvider>
   );
