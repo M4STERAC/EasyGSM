1642a3d4d1a6b055efe88baeb714f83d5449a85d
Matthew Manka <mrmanka0307@gmail.com>
2024-02-20 17:05:11 -0600

    moved footer info into the footer component, added comments, and cleaned up code

--- src/ui/components/Footer.js
+++ src/ui/components/Footer.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import logo from '../images/EasyGSMLogo.png';
 import '../css/Footer.css';
 
-const Footer = ({ children }) => (
+//Footer component loaded at the bottom of each page
+const Footer = () => (
   <div className="footer">
-    {children}
+    <div className="footer-logo">
+      <img src={logo} alt="EasyGSM Logo" />
+    </div>
+    <div className="footer-text">
+      <p>EasyGSM is an open source software. Please credit
+      https://github.com/M4STERAC/EasyGSM as the first author of the
+      program in all areas where credit is due.</p>
+
+      <p>Report an issue <a href="https://github.com/M4STERAC/EasyGSM/issues/new">HERE</a></p>
+    </div>
   </div>
 );
 
