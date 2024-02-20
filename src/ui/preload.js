d7d624b99ba42d324ce30cc5b3542ac7e8cb1b79
Matthew Manka <mrmanka0307@gmail.com>
2024-02-20 17:04:16 -0600

    added comments

--- src/ui/preload.js
+++ src/ui/preload.js
@@ -1,5 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
+// Expose protected methods that allow the renderer process to use the ipcRenderer without exposing the entire object
 contextBridge.exposeInMainWorld('electron', {
     invoke: (channel, data) => ipcRenderer.invoke(channel, data)
 });
