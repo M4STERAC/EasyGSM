5c542f221937488969e208425e9efb678e6e7a09
Matthew Manka <mrmanka0307@gmail.com>
2024-02-20 17:03:13 -0600

    added comments

--- src/ui/index.js
+++ src/ui/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+//Entry point for webpack bundle which is loaded by the index.html file
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
