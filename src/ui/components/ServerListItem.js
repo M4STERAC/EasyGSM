4c0b7922d0230c7ed593eb532e059297c17479a5
Matthew Manka <mrmanka0307@gmail.com>
2024-02-20 20:03:27 -0600

    Added comments and cleaned up code

--- src/ui/components/ServerListItem.js
+++ src/ui/components/ServerListItem.js
@@ -1,17 +1,13 @@
 import React from "react";
 import "../css/ServerListItem.css";
 
+//Lists the servers stored in electron store
 const ServerListItem = ({ server, onClick }) => {
   return (
     <p onClick={onClick}>
       <a href="javascript:void(0)" >
         {server.game} - {server.name}{" "}
-        <span
-          className={
-            server.status === "Running" ? "running" : "down"
-          }>
-          {server.status}
-        </span>
+        <span className={ server.status === "Running" ? "running" : "down" }>{server.status}</span>
       </a>
     </p>
   );
