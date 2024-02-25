1319b500049ad1dcf9b7115c8247b35e095acb6e
Matthew <mrmanka0307@gmail.com>
2024-02-24 18:34:59 -0600

    added route to view license

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -5,6 +5,7 @@ import NotFound from "./pages/NotFound";
 import UpdateDatabase from "./pages/UpdateDatabase";
 import Footer from "./components/Footer";
 import FirstLoad from "./pages/FirstLoad";
+import License from "./pages/License";
 import "./css/General.css";
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
@@ -27,6 +28,7 @@ const App = () => {
           <Route path="/first-launch" element={<FirstLoad />} />
           <Route path="/add-server" element={<UpdateDatabase />} />
           <Route path="/edit-server" element={<UpdateDatabase />} />
+          <Route path="/license" element={<License />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
