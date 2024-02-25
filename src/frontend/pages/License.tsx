522c3d9cd8329eb4ee3a9b678265753936c33df6
Matthew <mrmanka0307@gmail.com>
2024-02-24 18:34:36 -0600

    Created to display license agreement

---
+++ src/frontend/pages/License.tsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import Card from '../components/Card';
+import '../css/Card.css';
+
+const License = () => {
+    const [licenseText, setLicenseText] = useState('');
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchLicenseText = async () => {
+            try {
+                const response = await fetch('../../../license.md');
+                const text = await response.text();
+                setLicenseText(text);
+            } catch (error) {
+                console.error('Error fetching license text:', error);
+            }
+        };
+
+        fetchLicenseText();
+    }, []);
+
+    return (
+        <Card>
+            <p>{licenseText ?? navigate('*')}</p>
+        </Card>
+    );
+};
+
+export default License;
