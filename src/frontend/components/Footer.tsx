c7c4096c8e31540bfdd11812b92c9099da2da95a
Matthew <mrmanka0307@gmail.com>
2024-02-24 18:34:47 -0600

    Added links

--- src/frontend/components/Footer.tsx
+++ src/frontend/components/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from '../images/EasyGSMLogo.png';
 import '../css/Footer.css';
+import { Link } from 'react-router-dom';
 
 //Footer component loaded at the bottom of each page
 const Footer = () => (
@@ -9,10 +10,10 @@ const Footer = () => (
       <img src={logo} alt="EasyGSM Logo" />
     </div>
     <div className="footer-text">
-      <p>EasyGSM is an open source software. Please credit
-      https://github.com/M4STERAC/EasyGSM as the first author of the
-      program in all areas where credit is due.</p>
-
+      <p>EasyGSM is an open source software used to make automating commissioning, managing, and decommissioning game servers easy.</p>
+      <p>Developed by: <a href="https://github.com/M4STERAC">Matthew Manka</a></p>
+      <p>View source code: <a href="https://github.com/M4STERAC/EasyGSM">HERE</a></p>
+      <p>Please read the <Link to="/license">license</Link></p>
       <p>Report an issue <a href="https://github.com/M4STERAC/EasyGSM/issues/new">HERE</a></p>
     </div>
   </div>
