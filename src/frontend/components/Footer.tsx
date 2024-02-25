6140ed72328549fc441468f142d932d27205fd08
Matthew <mrmanka0307@gmail.com>
2024-02-25 11:05:29 -0600

    extracted link styles

--- src/frontend/components/Footer.tsx
+++ src/frontend/components/Footer.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import logo from '../images/EasyGSMLogo.png';
-import '../css/Footer.css';
 import { Link } from 'react-router-dom';
+import '../css/Footer.css';
+import '../css/Links.css';
 
 //Footer component loaded at the bottom of each page
 const Footer = () => (
@@ -11,10 +12,11 @@ const Footer = () => (
     </div>
     <div className="footer-text">
       <p>EasyGSM is an open source software used to make automating commissioning, managing, and decommissioning game servers easy.</p>
-      <p>Developed by: <a href="https://github.com/M4STERAC">Matthew Manka</a></p>
-      <p>View source code: <a href="https://github.com/M4STERAC/EasyGSM">HERE</a></p>
-      <p>Please read the <Link to="/license">license</Link></p>
-      <p>Report an issue <a href="https://github.com/M4STERAC/EasyGSM/issues/new">HERE</a></p>
+      <p>Developed by: <a className="link" href="https://github.com/M4STERAC">Matthew Manka</a></p>
+      <p>View source code: <a className="link" href="https://github.com/M4STERAC/EasyGSM">HERE</a></p>
+      <p>Please read the <Link className="link" to="/license">license</Link></p>
+      <p>Go back <Link className="link" to="/">Home</Link></p>
+      <p>Report an issue <a className="link" href="https://github.com/M4STERAC/EasyGSM/issues/new">HERE</a></p>
     </div>
   </div>
 );
