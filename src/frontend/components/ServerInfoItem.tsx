30d6f35d2c774031656225c1a8de4aac128b7592
Matthew <mrmanka0307@gmail.com>
2024-02-25 11:05:44 -0600

    moved buttons to mainpage

--- src/frontend/components/ServerInfoItem.tsx
+++ src/frontend/components/ServerInfoItem.tsx
@@ -1,50 +1,13 @@
 import React, { useState, useContext } from "react";
 import { StoreContext } from "../Store";
-import { CSSTransition } from "react-transition-group";
+
 import { Link } from "react-router-dom";
 import "../css/ServerInfoItem.css";
+import "../css/Links.css";
 
 //When a server is selected from ServerListItem component, this component will display the properties of that selected server
 const ServerInfoItem = ({ selectedServer }: { selectedServer: Server }) => {
   const [state, setState] = useContext(StoreContext);
-  const [buttonState, setButtonState] = useState("Down");
-  const [isButtonDisabled, setButtonDisabled] = useState(false);
-
-
-  //Function to start and stop selected server
-  const executeServer = (channel: string, server: Server) => {
-    window.electron.invoke(channel, server)
-      .then((output: any) => console.log(output))
-      .catch((error: any) => console.error(error));
-  };
-
-
-  //Function executed when the START button is clicked
-  const handleStartButtonClick = () => {
-    setTimeout(() => setButtonDisabled(false), 3000);
-    executeServer("start-server", selectedServer);
-    setButtonState("Running");
-    setState((prevState: any) => {
-      const serverIndex = prevState.serverList.findIndex((server: Server) => server.id === selectedServer.id);
-      const newServerList = [...prevState.serverList];
-      newServerList[serverIndex] = { ...newServerList[serverIndex], status: "Running" };
-      return { ...prevState, serverList: newServerList };
-    });
-  };
-
-
-  //Function executed when the STOP button is clicked
-  const handleStopButtonClick = () => {
-    setTimeout(() => setButtonDisabled(false), 3000);
-    executeServer("stop-server", selectedServer);
-    setButtonState("Down");
-    setState((prevState: any) => {
-      const serverIndex = prevState.serverList.findIndex((server: Server) => server.id === selectedServer.id);
-      const newServerList = [...prevState.serverList];
-      newServerList[serverIndex] = { ...newServerList[serverIndex], status: "Down" };
-      return { ...prevState, serverList: newServerList };
-    });
-  };
 
   return (
     <div>
@@ -60,22 +23,7 @@ const ServerInfoItem = ({ selectedServer }: { selectedServer: Server }) => {
         <li>UDP O Ports: {selectedServer.ports.udpoutbound}</li>
       </ul>
 
-      <Link className="edit" to={"/edit-server"}>Edit</Link>
-
-      <div className="button-container">
-        <CSSTransition in={buttonState === "Down"} timeout={300} classNames="start-button" unmountOnExit>
-          <button className="start-button" onClick={() => handleStartButtonClick()} disabled={isButtonDisabled}>
-            Start
-          </button>
-        </CSSTransition>
-
-        <CSSTransition in={buttonState !== "Down"} timeout={300} classNames="stop-button" unmountOnExit>
-          <button className="stop-button" onClick={() => handleStopButtonClick()} disabled={isButtonDisabled}>
-            Stop
-          </button>
-        </CSSTransition>
-      </div>
-
+      {state.selectedServer.status === "Down" ? <Link className="link" to={"/edit-server"}>Edit</Link> : <p>Cannot edit will running</p>}
     </div>
   );
 };
