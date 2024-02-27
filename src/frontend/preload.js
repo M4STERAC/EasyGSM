b628e09398a9c437abe3640c0d4405a8c8911b83
Matthew <mrmanka0307@gmail.com>
2024-02-27 11:45:41 -0600

    removed unneeded methods from elctron ipc

--- src/frontend/preload.js
+++ src/frontend/preload.js
@@ -2,5 +2,4 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electron', {
     invoke: (channel, data) => ipcRenderer.invoke(channel, data),
-    getPid: (callback) => ipcRenderer.on('get-pid', (_event, value) => callback(value)),
   });
