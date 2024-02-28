2c2048ece579e38334974d239695dabf3dbb6216
Matthew <mrmanka0307@gmail.com>
2024-02-27 18:10:56 -0600

    created event listeners to execute title bar buttons

--- src/frontend/frame.js
+++ src/frontend/frame.js
@@ -1,21 +1,16 @@
-const remote = require('electron').remote;
 
+const closeBtn = document.getElementById('close')
+const minimizeBtn = document.getElementById('minimize')
+const maximizeBtn = document.getElementById('maximize')
 
-const closeBtn = document.querySelector('close');
-const minimizeBtn = document.querySelector('minimize');
-const maximizeBtn = document.querySelector('maximize');
+closeBtn.addEventListener('click', () => {
+    window.electron.close();
+})
 
-const win = remote.getCurrentWindow();
+minimizeBtn.addEventListener('click', () => {
+  window.electron.minimize();
+})
 
-closeBtn.addEventListener('click', function (e) {
-  win.close();
-});
-
-minimizeBtn.addEventListener('click', function (e) {
-  win.minimize();
-});
-
-maximizeBtn.addEventListener('click', function (e) {
-  if (win.isMaximized()) win.unmaximize(); 
-  else win.maximize();
-});
+maximizeBtn.addEventListener('click', () => {
+    window.electron.maximize();
+})
