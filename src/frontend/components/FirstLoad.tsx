8c211f8f1eae8c683f4897db0f9804994c858f47
Matthew <mrmanka0307@gmail.com>
2024-02-28 19:10:11 -0600

    Moved FirstLoad to Components

--- src/frontend/components/FirstLoad.tsx
+++ src/frontend/components/FirstLoad.tsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { StoreContext } from "../Store";
 import { useNavigate } from "react-router-dom";
 import { sanitizeFilePath, validateFilePath } from '../utils/dataValidation';
-import Card from '../components/Card';
+import Card from './Card';
 import '../css/ButtonStyles.css';
 import "../css/Forms.css";
 
