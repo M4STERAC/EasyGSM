9d2b5402dd993bb85b117ad41ec911c4c2e33d5a
Matthew <mrmanka0307@gmail.com>
2024-02-29 00:04:02 -0600

    added cssbaseline for normalization

--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import CssBaseline from '@mui/material/CssBaseline';
+
 
 //Entry point for webpack bundle which is loaded by the index.html file
 const rootElement: HTMLElement | null = document.getElementById('root');
@@ -8,6 +10,7 @@ if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <React.StrictMode>
+      <CssBaseline />
       <App />
     </React.StrictMode>
   );
