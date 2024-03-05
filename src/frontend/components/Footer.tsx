13d2a8018ec784228a9a66c11d5bb7cd2e6252bc
Matthew <mrmanka0307@gmail.com>
2024-03-05 09:07:47 -0600

    removed image from footer

--- src/frontend/components/Footer.tsx
+++ src/frontend/components/Footer.tsx
@@ -7,9 +7,6 @@ import '../css/Links.css';
 //Footer component loaded at the bottom of each page
 const Footer = () => (
   <div className="footer">
-    <div className="footer-logo">
-      <img src={logo} alt="EasyGSM Logo" />
-    </div>
     <div className="footer-text">
       <p>EasyGSM is an open source software used to make automating commissioning, managing, and decommissioning game servers easy.</p>
     </div>
