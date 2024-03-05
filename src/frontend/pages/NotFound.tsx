a28e3d12e9369c80a42b7067aa5e4caa5fa9a7e0
Matthew <mrmanka0307@gmail.com>
2024-03-05 12:15:46 -0600

    Now uses theme colors

--- src/frontend/pages/NotFound.tsx
+++ src/frontend/pages/NotFound.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useTheme } from '@mui/material/styles';
+
 
 //Default 404 page
 const NotFound = () => {
-  
+  const theme = useTheme();
+
   return (
-  <div>
-    <p>404. The route you entered could not be found. Sorry!</p>
+  <div style={{ justifyContent: 'center', alignItems: 'center', textAlign: 'center', color: theme.palette.common.white }}>
+    <h2>404</h2>
+    <p>The route you entered could not be found. Sorry!</p>
     <p>If this should have gone somewhere, please submit an issue <a href="https://github.com/M4STERAC/EasyGSM/issues/new">HERE</a></p>
     <p>-EasyGSM Team</p>
-    <Link to="/">Back to Main</Link>
   </div>
 )};
 
