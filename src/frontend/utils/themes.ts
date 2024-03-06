e3352ef2e7c3f14e83b8a691985712d6d420711c
Matthew <mrmanka0307@gmail.com>
2024-03-06 10:48:39 -0600

    updated theme colors

--- src/frontend/utils/themes.ts
+++ src/frontend/utils/themes.ts
@@ -3,9 +3,21 @@ import { Theme, createTheme } from '@mui/material/styles';
 
 export const darkTheme: Theme = createTheme({ palette: {
     mode: 'dark', 
+    primary: {
+      main: '#1976d2',
+      light: '#42a5f5',
+      dark: '#1565c0',
+      contrastText: '#fff',
+    },
+    secondary: {
+      main: '#9c27b0',
+      light: '#ba68c8',
+      dark: '#7b1fa2',
+      contrastText: '#fff',
+    },
     background: {
-      default: '#0d1117',
-      paper: '#f9f9f9',
+      default: '#171b1f',
+      paper: '#262f39',
     },
     text: {
       primary: '#fff',
