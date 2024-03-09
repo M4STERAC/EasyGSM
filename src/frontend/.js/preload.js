186bd09a0aed6744c0e5add197a84564700af026
Matthew <mrmanka0307@gmail.com>
2024-03-09 13:33:38 -0600

    added dependency download api

--- src/frontend/.js/preload.js
+++ src/frontend/.js/preload.js
@@ -2,6 +2,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electron', {
   invoke: (channel, data) => ipcRenderer.invoke(channel, data),
+  dlDependencies: () => ipcRenderer.send('install-dependencies'),
   close: () => ipcRenderer.send('app/close'),
   minimize: () => ipcRenderer.send('app/minimize'),
   maximize: () => ipcRenderer.send('app/maximize'),
